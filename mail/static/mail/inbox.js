316622f066c3216b94990cfe89de0ce428567074
GastonUC <gacox.magallanes@gmail.com>
2025-01-08 23:38:19 -0300

    first commit

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+}
+
+function load_mailbox(mailbox) {
+  
+  // Display the emails in the mailbox
+  // fetch(`/emails/${mailbox}`)
+  fetch(`/emails/inbox`)
+  .then(response => response.json())
+  .then(emails => {
+    document.querySelector('#emails-view').style.display = 'block';
+    document.createElement('p').innerText = emails;
+
+  })
+
+  // Show the mailbox and hide other views
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
